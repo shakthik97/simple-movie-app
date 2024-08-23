00416d0a17b0171e256f002b8b567edb849f165f
Chathuranga Shakthi <chathurangashakthi.k@gmail.com>
2024-08-23 11:28:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import {useEffect} from 'react';
 
-const API_URL = 'http://www.omdbapi.com?apikey=1bf6a6e1';
+// const API_URL = 'http://www.omdbapi.com?apikey=1bf6a6e1';
 
-const app = () =>{
-  useEffect(() => {
+// const app = () =>{
+//   useEffect(() => {
 
-  },[]);
+//   },[]);
 
-  return (
-    <h1>app</h1>
-  );
-}
+//   return (
+//     <h1>app</h1>
+//   );
+// }
 
 function App() {
   return (
